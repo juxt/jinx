9293110d8ebfeffcedcbd729b15047c9a3d335b3
sunitakawane <sunitakawane@gmail.com>
2019-06-20 15:34:19 +0530

    add regex cljs tests

--- test/juxt/jsonschema/regex_cljc_test.cljs
+++ test/juxt/jsonschema/regex_cljc_test.cljs
@@ -16,11 +16,27 @@
 ;       "query" "debug=true"
 ;       "fragment" "bar")))
 
-; (deftest addr-spec-test
-;   (let [m (regex/matched regex/addr-spec "mal@juxt.pro")]
-;     (are [group expected] (= expected (regex/re-group-by-name m group))
-;       "localpart" "mal"
-;       "domain" "juxt.pro")))
+(deftest addr-spec-test
+  (are [x y] (= y (regex/parse "addr-spec-test" x))
+    "mal@juxt.pro"
+    ["mal" "juxt.pro"]))
 
+(deftest iri-test
+  (are [x y] (= y (regex/parse "iri-test" x))
+    "https://jon:password@juxt.pro:8080/site/index.html?debug=true#bar"
+    ["https" "jon:password"  "juxt.pro" "8080" "/site/index.html" "debug=true"  "bar" ]
+    
+    "/happy/path"
+    [nil nil nil nil "/happy/path" nil nil]
 
+    "relative/path"
+    [nil nil nil nil "relative/path" nil nil]
+
+    "http://user:password@example.com:8080/path?query=value#fragment"
+    ["http" "user:password" "example.com" "8080" "/path" "query=value" "fragment"]
+    
+    ; "http://example.com"
+    ; ["http" nil nil "example.com" nil nil nil nil]
+    
+    ))
 ;; TODO: ipv6 tests from rfc2234
