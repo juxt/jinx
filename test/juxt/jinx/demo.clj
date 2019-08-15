328d6ad0b772b3abeef256f877b2aeabd9158a62
Malcolm Sparks <mal@juxt.pro>
2019-08-15 15:31:25 +0100

    Add demo to show how to use jinx

---
+++ test/juxt/jinx/demo.clj
@@ -0,0 +1,26 @@
+(ns juxt.jinx.demo
+  (:require
+   [juxt.jinx.schema :refer [schema]]
+   [juxt.jinx.validate :refer [validate]]))
+
+
+(comment
+  (validate
+   {"firstName" "John"
+    "lastName" "Doe"
+    "age" 21}
+   (schema
+    {"$id" "https://example.com/person.schema.json"
+     "$schema" "http://json-schema.org/draft-07/schema#"
+     "title" "Person"
+     "type" "object"
+     "properties"
+     {"firstName"
+      {"type" "string"
+       "description" "The person's first name."}
+      "lastName"
+      {"type" "string"
+       "description" "The person's last name."}
+      "age" {"description" "Age in years which must be equal to or greater than zero."
+             "type" "integer"
+             "minimum" 0}}})))
